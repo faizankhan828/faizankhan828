94a1484907db2b0f9b5696dee906c77d6aed59ad
faizankhan828 <faizankhan15658@gmail.com>
2023-03-15 21:13:45 +0500

    first

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path
+from basic_app import views
+
+#TEMPLATE TAGGING
+
+
+app_name = 'basic_app'
+
+urlpatterns = [
+    re_path('', views.relative, name='relative'),
+    re_path(r'^other/$', views.other, name='other'),
+]
